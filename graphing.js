a4d3f81b0b97f5bc4636e2febfce25a2b2d0be76
Chris Lynch <lynchch4@gmail.com>
2023-04-22 05:59:36 -0400

    added functionality to dropdown menu

--- graphing.js
+++ graphing.js
@@ -9,6 +9,7 @@ const data2 = d3.json(url2).then(function(data2) {console.log(data2)});
 const data3 = d3.json(url3).then(function(data3) {console.log(data3)});
 
 var industry_list = []
+var industry_list2 = []
 var fy14_list = []
 var fy15_list = []
 var fy16_list = []
@@ -33,83 +34,102 @@ function start() {
         
 
         for (let i = 0; i < data.length; i++)    
-            industry_list.push(data[i].industry)
-        for (let i = 0; i < data.length; i++)    
-            fy14_list.push(data[i].fy14)    
-
+            industry_list.push(data[i].industry)    
+        console.log(industry_list)
         // select drop down from html
-            let selection = d3.select("#selDataset");
+        let selection = d3.select("#selDataset");
 
         // map data and apply to drop down
-            industry_list.map(function(set) {
+        industry_list.map(function(set) {
             return selection.append("option").text(set)
+        
         });
-        graph(industry_list[0])
+
     });
+    graph("Construction")
 }
 
-function graph() {
-    document.addEventListener('DOMContentLoaded', function () {
-        d3.json(url).then(function(data) {
-            for (let i = 0; i < data.length; i++)    
-                fy14_list.push(data[i].fy14)
-            for (let i = 0; i < data.length; i++)    
-                fy15_list.push(data[i].fy15)
-            for (let i = 0; i < data.length; i++)    
-                fy16_list.push(data[i].fy16)
-            for (let i = 0; i < data.length; i++)
-                fy17_list.push(data[i].fy17)  
-            for (let i = 0; i < data.length; i++)    
-                fy18_list.push(data[i].fy18)       
-            for (let i = 0; i < data.length; i++)    
-                fy19_list.push(data[i].fy19)
-            for (let i = 0; i < data.length; i++)    
-                fy20_list.push(data[i].fy20)
-            for (let i = 0; i < data.length; i++)    
-                fy21_list.push(data[i].fy21)        
-                
-
-            const chart = Highcharts.chart('bar', {
-                chart: {
-                    type: 'bar'
-                },
-                title: {
-                    text: 'Unemployment by Industry Type'
-                },
-                xAxis: {
-                    categories: industry_list
-                },
-                yAxis: {
-                    title: {
-                        text: 'Unemployment in 1000s'
-                    }
-                },
-                series: [{
-                    name: "FY15",   
-                    data: fy15_list
-                }, {
-                    name: "FY16",   
-                    data: fy16_list
-                }, {
-                    name: "FY17",   
-                    data: fy17_list
-                }, {
-                    name: "FY18",   
-                    data: fy18_list
-                }, {
-                    name: "FY19",   
-                    data: fy19_list
-                }, {
-                    name: "FY20",   
-                    data: fy20_list
-                }, {
-                    name: "FY21",   
-                    data: fy21_list
-                }]
-            });
+function graph(set) {
+    
+    d3.json(url).then(function(data) {
+        function indSelect(inds) {
+            return inds.industry == set
+        };    
+        let filter = data.filter(indSelect)[0]
+        console.log(filter)
+      
+        //select panel from html
+        let body = d3.select("#industry-data");        
+        info = Object.entries(filter);
+        // Clear previous panel body
+   
+        body.html("");
+        // apply key value pair of metadata to panel body
+        info.map(function([key, value]) {
+            return body.append("p").text(`${key}:         ${value}`)
         });
     });
 
+    //document.addEventListener('DOMContentLoaded', () => {
+            d3.json(url).then(function (data) {
+                function indSelect(inds) {
+                    return inds.industry == set;
+                };
+                let filter = data.filter(indSelect)[0];
+
+                console.log(filter);
+
+                info = Object.entries(filter);
+
+                const chart = Highcharts.chart('bar', {
+                    chart: {
+                        type: 'bar'
+                    },
+                    title: {
+                        text: 'Unemployment by Industry Type'
+                    },
+                    xAxis: {
+                        categories: [filter.industry],
+                        labels: {
+                            enabled: false
+                        },
+                        title: {
+                            text: filter.industry
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Unemployment in 1000s'
+                        }
+                    },
+                    series: [{
+                        name: "FY15",
+                        data: [filter.fy15]
+                    }, {
+                        name: "FY16",
+                        data: [filter.fy16]
+                    }, {
+                        name: "FY17",
+                        data: [filter.fy17]
+                    }, {
+                        name: "FY18",
+                        data: [filter.fy18]
+                    }, {
+                        name: "FY19",
+                        data: [filter.fy19]
+                    }, {
+                        name: "FY20",
+                        data: [filter.fy20]
+                    }, {
+                        name: "FY21",
+                        data: [filter.fy21]
+                    }]
+                });
+            });
+       // });
+}
+
+function graph2() {
     document.addEventListener('DOMContentLoaded', function () {
         d3.json(url2).then(function(data2) {
             for (let i = 0; i < data2.length; i++)    
@@ -175,7 +195,9 @@ function graph() {
                     categories: date_g_list
                 },
                 yAxis: {
-
+                    title: {
+                        text: 'Unemployment Rate'
+                    }
                 },
                 series: [{
                     name: 'Women',
@@ -191,8 +213,9 @@ function graph() {
 
 
 start();
-graph()
+
+graph2();
 
 function optionChanged(Data) {
-    start(Data);
+    graph(Data);
 };
